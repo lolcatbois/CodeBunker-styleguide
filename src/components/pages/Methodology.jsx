c775ce5607121ea9c2d0a3e839927f9ebc77ee87
simon <soelder@hotmail.com>
2021-02-09 18:24:22 +0100

    Initial methodology page

---
+++ src/components/pages/Methodology.jsx
@@ -0,0 +1,19 @@
+import CollapsibleCode from "../modules/CollapsibleCode";
+
+
+export default function Methodology() {
+    return (
+        <div className="page">
+            <section>
+                <h1>COLORS</h1>
+                <p>
+                    We mainly focus on various shades of gray (less than 50),
+                    with a dark background. a highlight color and an accent color.
+                </p>
+            </section>
+
+            <section>
+            </section>
+        </div>
+    );
+}
